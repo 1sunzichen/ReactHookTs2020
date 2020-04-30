acc86652191d71fd0574adfc634cbe0e651ce9c0
sjq <3023493319@qq.com>
2020-04-30 14:52:41 +0800

    fix

---
+++ my-app/src/components/Menu/menu.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+describe('test Menu and MenuItem',()=>{
+    it('should render correct Me',()=>{
+
+    })
+    it("click items should",()=>{
+
+    })
+})
